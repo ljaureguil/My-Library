712bcf674a8db4099ce45b1deb79b93c51950f13
ljaureguil <33967760+ljaureguil@users.noreply.github.com>
2024-03-21 12:22:31 -0700

    Create threeBase.js

---
+++ threeBase.js
@@ -0,0 +1,102 @@
+         var camera, scene, renderer, ainicio, cods = '';
+         var geometry, material, mesh;
+
+
+         loadDoc("https://raw.githubusercontent.com/ljaureguil/My-Library/master/three.min.js", function(r) {
+
+             insertScript(r)
+
+             loadDoc("https://cdn.rawgit.com/chandlerprall/ThreeCSG/aefe3778/ThreeCSG.js", function(r2) {
+
+                 insertScript(r2)
+                 loadDoc("https://raw.githubusercontent.com/ljaureguil/My-Library/master/MyLib.js", function(r3) {
+                     insertScript(r3)
+
+                     loadDoc("https://raw.githubusercontent.com/ljaureguil/My-Library/master/TrackballControls.js", function(t) {
+
+                         insertScript(t)
+
+                         setTimeout(function() {
+                             init();
+                         }, 11);
+
+                     })
+                 })
+             })
+
+         });
+          //src="//cdn.rawgit.com/mrdoob/three.js/master/build/three.min.js" onload="init"
+
+         function init() {
+
+             camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 1000);
+             camera.position.z = 100;
+
+             scene = new THREE.Scene();
+             renderer = new THREE.WebGLRenderer({
+                 antialias: true,
+                 alpha: true
+             });
+             renderer.setSize(window.innerWidth, window.innerHeight);
+             document.body.appendChild(renderer.domElement);
+
+             var directionalLight = new THREE.DirectionalLight(0xffffff, 1.35, 1000);
+
+             directionalLight.position.y = 500;
+             directionalLight.position.x = 400;
+             directionalLight.position.z = 300;
+             directionalLight.castShadow = true;
+             directionalLight.target = scene;
+             scene.add(directionalLight);
+
+             var spotLight = new THREE.SpotLight(0xaaaaaa);
+             spotLight.castShadow = true;
+
+             spotLight.shadow.mapSize.width = 1024;
+             spotLight.shadow.mapSize.height = 1024;
+
+             spotLight.shadow.camera.near = 500;
+             spotLight.shadow.camera.far = 4000;
+             spotLight.shadow.camera.fov = 300;
+             bulbMat = new THREE.MeshStandardMaterial({
+                 emissive: 0xffffff,
+                 emissiveIntensity: 1,
+                 color: 0xff0000
+             });
+
+             var light = new THREE.AmbientLight(0xffffff, .65); // soft white light
+             scene.add(light);
+             controls = new THREE.TrackballControls(camera, renderer.domElement);
+             animate();
+
+         }
+
+         function animate() {
+             requestAnimationFrame(animate);
+             render();
+         }
+
+         function render() {
+             controls.update();
+             eval(cods)
+             renderer.render(scene, camera);
+         }
+
+         function loadDoc(url, callback) {
+             var xhttp = new XMLHttpRequest();
+
+             xhttp.open("GET", url, false);
+             xhttp.send();
+             callback(xhttp.responseText);
+
+         }
+
+         function insertScript(script) {
+             var head = document.head;
+
+             var createScript = document.createElement('script');
+             createScript.innerHTML = script;
+             createScript.setAttribute('defer', '');
+
+             head.appendChild(createScript);
+         }
